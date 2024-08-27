616245aece3099afc3ca102400cf67ae64addb04
Sakshi Sawarkar <101932054+sakshi-sawarkar@users.noreply.github.com>
2024-08-27 15:56:55 +0530

    Add files via upload

---
+++ reversing.py
@@ -0,0 +1,15 @@
+#def reversearray (arr):
+    #reversed_arr = arr[::-1]
+
+    #print("reversed array:", end = " ")
+    #for i in reversed_arr:
+        #print(i, end=" ")
+
+#original_arr = [1,2,3,4,5]
+#reversearray(original_arr)
+
+original_array = [1,2,3,4,5]
+
+reversed_array = list(reversed(original_array))
+
+print(reversed_array)
